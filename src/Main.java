de568db34bbf2129e6addd2c91ba53858609ec40
Ilona <dobro.ilona@yandex.ru>
2023-03-14 13:48:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 500;
+        int bonusPerMile = 20;
+
+        int bonuses = amount / bonusPerMile;
+
+        System.out.println(bonuses);
+
+    }
+}
